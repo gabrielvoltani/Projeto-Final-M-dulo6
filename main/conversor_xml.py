ca48e20e55e334e470104d9ffb83ca181eb1bf73
arjona-jonas <arjonasjonascont@gmail.com>
2023-12-07 21:37:03 -0300

    Fix: Transformando script de XML em função

--- main/conversor_xml.py
+++ main/conversor_xml.py
@@ -1,18 +1,17 @@
 import csv
+import xml.etree.ElementTree as ET
 
 with open('../files/data.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     data = [row for row in reader]
 
-import xml.etree.ElementTree as ET
-
-root = ET.Element('ListaDeProdutos')
-for row in data:
-    item = ET.SubElement(root, 'produto')
-    for key, value in row.items():
-        child = ET.SubElement(item, key)
-        child.text = value
-
-
-tree = ET.ElementTree(root)
-tree.write('../files/data.xml')
+def escrever_arquivo_xml(data,caminho_arquivo):
+    root = ET.Element('ListaDeProdutos')
+    for row in data:
+        item = ET.SubElement(root, 'produto')
+        for key, value in row.items():
+            child = ET.SubElement(item, key)
+            child.text = value
+    tree = ET.ElementTree(root)
+    tree.write(caminho_arquivo)
+    print(f"Arquivo XML '{caminho_arquivo}' criado com sucesso!")
